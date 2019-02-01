bc1bff447ac28f1c1d47f5afdc5651bb83328c94
george518 <haodaquan2008@163.com>
2019-02-01 15:17:53 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/************************************************************
+** @Description: file
+** @Author: georgehao
+** @Date:   2019-02-01 15:05:37
+** @Last Modified by:   georgehao
+** @Last Modified time: 2019-02-01 15:07:54
+*************************************************************/
+
+require_once __DIR__.'/vendor/autoload.php';
+use Ali\TmallSpider;
+
+echo TmallSpider::url("tmall_url");
---
+++ src/TmallSpider.php
@@ -0,0 +1,24 @@
+<?php
+
+/************************************************************
+** @Description: 抓取天猫内容页商品
+** @Author: georgehao
+** @Date:   2019-02-01 14:22:01
+** @Last Modified by:   georgehao
+** @Last Modified time: 2019-02-01 15:04:20
+*************************************************************/
+namespace Ali;
+
+class TmallSpider
+{
+	
+	function __construct()
+	{
+		echo "Test";
+	}
+
+	public static function url($tmall_id)
+	{
+		echo $tmall_id;
+	}
+}
